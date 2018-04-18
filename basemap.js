46d603af1910ab0b7ad3b09f02f981e66f2bff74
jessica balling <jessica.balling@student.uibk.ac.at>
2018-04-18 18:05:06 +0200

    URLs als Objekte für Karte dazu

--- basemap.js
+++ basemap.js
@@ -1,9 +1,28 @@
-// 
+// Rahmen für die Karte 
 let myMap = L.map("mapdiv");
-// Hintergrund aus dem Internet geladen 
-let myLayer = L.tileLayer ("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+let myLayers= {
+    osm: L.tileLayer (
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    ),
+    geolandbasemap: L.tileLayer(
+        "https://maps.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png"
+    ),
+    bmapoverlay: L.tileLayer (
+        "https://maps.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png"
+    ),
+    bmapgrau: L.tileLayer (
+        "https://maps.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png"
+    ),
+    bmaphidpi: L.tileLayer (
+        "https://maps.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jepg"
+    ),
+    bmaporthofoto30cm: L.tileLayer (
+        "https://maps.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jepg"
+    )
+};
 
-// alles zusammenhängen 
-myMap.addLayer(myLayer);
-// Koordinaten in eckigen Klammern
+
+// Layer zur Karte hinzufügen - zusammenbauen 
+myMap.addLayer(myLayers.osm);
+// Zentrum der Karte setzen 
 myMap.setView([47.267,11.383],11)
